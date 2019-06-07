8049761dab260d0d76e5628d6329fad065034e7c
booldook <booldook@gmail.com>
2019-06-07 11:38:46 +0900

    navi 구현

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,12 @@
 // navi-bar
 $(".navi > li").mouseover(function(){
 	$(this).find(".navi-bar").addClass("navi-bar-hover");
-	$(this).find(".navi-subs").css({"display":"flex", "opacity":0});
-	$(this).find(".navi-subs").stop().animate({"top":"3rem", "opacity":1}, 500);
+	$(this).find(".navi-subs").css({"display":"flex"});
+	$(this).find(".navi-subs").stop().animate({"top":"2.5rem", "opacity":1}, 500);
 });
 $(".navi > li").mouseleave(function(){
 	$(this).find(".navi-bar").removeClass("navi-bar-hover");
 	$(this).find(".navi-subs").stop().animate({"top":"5rem", "opacity":0}, 500, function(){
-		$(this),css({"display":"none"});
+		$(this).css({"display":"none"});
 	});
 });
