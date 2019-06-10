9bcc4237fbd9cc9978d971f09e8ca58afa8e2288
booldook <booldook@gmail.com>
2019-06-10 10:16:26 +0900

    top navi2

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,16 @@
 // navi-bar
 $(".navi > li").mouseover(function(){
+	switch($(this).index()) {
+		case 0:
+			$(this).find(".navi-subs").css({"display":"flex"});
+			break;
+		case 1:
+			$(this).find(".navi-subs").css({"display":"block"});
+			break;
+		default:
+			break;
+	}
 	$(this).find(".navi-bar").addClass("navi-bar-hover");
-	$(this).find(".navi-subs").css({"display":"flex"});
 	$(this).find(".navi-subs").stop().animate({"top":"2.5rem", "opacity":1}, 500);
 });
 $(".navi > li").mouseleave(function(){
