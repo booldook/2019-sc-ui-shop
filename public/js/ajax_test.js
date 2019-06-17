674a24f600adbc90bb72f729769cfb468b0c481b
booldook <booldook@gmail.com>
2019-06-17 11:03:11 +0900

    ajax_test.html

--- public/js/ajax_test.js
+++ public/js/ajax_test.js
@@ -1,14 +1,21 @@
+/* 
+/ajax/top/r : 1차카테고리
+/ajax/top/r2/3 : 1차카테고리의 1,2,3,4,5 의 2,3차 카테고리
+*/
+
 $("#bt1").click(function(){
 	$.ajax({
 		type: "get",
 		url: "/ajax/top/r",
 		dataType: "json",
 		success: function (res) {
-			console.log(res);
-			var html = '';
-			for(var i in res) {
-				html += res[i].id+' / '+res[i].catename+' / '+res[i].catelink+' / '+res[i].pos+'<br>';
+			var html = '<ul class="d-flex justify-content-center">';
+			for(var i=0 in res) {
+				//console.log(res[i].catename);
+				html += '<li class="border p-2" onclick="go(\''+res[i].catelink+'\');">'+res[i].catename+'</li>';
 			}
+			html += '</ul>';
+			//console.log(html);
 			$(".tx_area").html(html);
 		}
 	});
@@ -21,11 +28,6 @@ $("#bt2").click(function(){
 		dataType: "json",
 		success: function (res) {
 			console.log(res);
-			var html = '';
-			for(var i in res) {
-				html += res[i].id+' / '+res[i].catename+' / '+res[i].catelink+' / '+res[i].pos+'<br>';
-			}
-			$(".tx_area").html(html);
 		}
 	});
 });
@@ -33,3 +35,7 @@ $("#bt2").click(function(){
 $("#bt_reset").click(function(){
 	$(".tx_area").empty();
 });
+
+function go(url) {
+	location.href = url;
+}
